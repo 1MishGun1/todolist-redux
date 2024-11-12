a5080da579a8ed5f8eedd7ffc992c83f25c7323d
1MishGun1 <mikhail.kudrov@gmail.com>
2024-11-12 17:16:53 +0300

    add load and error message

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Styles from "./App.css";
 import { useState, useEffect } from "react";
 import { TodoList } from "./components/TodoList";
 import { InputField } from "./components/InputField";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addNewTask, fetchTasks } from "./store/taskSlice";
 
 function App() {
   const [text, setText] = useState("");
   const dispatch = useDispatch();
+  const { status, error } = useSelector((state) => state.tasks);
 
   const addTask = () => {
     dispatch(addNewTask({ text }));
@@ -21,6 +22,10 @@ function App() {
   return (
     <div className={Styles.App}>
       <InputField text={text} setText={setText} handleSubmit={addTask} />
+
+      {status === "load..." && <h2>Loading data</h2>}
+      {error === <h2>Error: {error}</h2>}
+
       <TodoList />
     </div>
   );
